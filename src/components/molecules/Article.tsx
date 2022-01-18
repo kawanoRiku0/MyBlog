e768f471f74fd8293d080336e7f5f142ff24eaa2
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-18 23:12:27 +0900

    fix: Articleコンポーネントのレイアウトを修正

--- src/components/molecules/Article.tsx
+++ src/components/molecules/Article.tsx
@@ -9,12 +9,12 @@ const noImage = "/images/no-image.jpg";
 
 const Article: FC<Props> = ({ article }) => {
   return (
-    <div className=" rounded-xl shadow-lg overflow-hidden  md:w-[29%] m-3 w-full hover:cursor-pointer hover:scale-105 transition-all">
+    <div className=" rounded-xl shadow-xl overflow-hidden  md:w-[29%] m-3 w-full hover:cursor-pointer hover:scale-105 transition-all">
       {/* Imageコンポーネントを使用することが望ましい、一時的にimgタグ　 */}
       <img
         src={article.eye_catch?.url || noImage}
         alt="画像"
-        className="w-full"
+        className="w-full max-h-52 object-cover"
       />
       <div className="p-6">
         <div className="text-xl py-6">{article.title || "記事タイトル"}</div>
