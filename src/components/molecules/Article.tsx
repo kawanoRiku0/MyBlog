d7fffa2bc1720015d06fb07d43b68d092d9fa684
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-18 20:42:13 +0900

    feat: Articleコンポーネント作成

---
+++ src/components/molecules/Article.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+import { Article } from "types/article";
+
+type Props = {
+  article: Article;
+};
+
+const noImage = "/images/no-image.jpg";
+
+const Article: FC<Props> = ({ article }) => {
+  return (
+    <div className=" rounded-xl shadow-lg overflow-hidden  md:w-[29%] m-3 w-full hover:cursor-pointer hover:scale-105 transition-all">
+      {/* Imageコンポーネントを使用することが望ましい、一時的にimgタグ　 */}
+      <img
+        src={article.eye_catch?.url || noImage}
+        alt="画像"
+        className="w-full"
+      />
+      <div className="p-6">
+        <div className="text-xl py-6">{article.title || "記事タイトル"}</div>
+        <div>
+          <span className=" bg-gray-300  rounded-full px-4 py-2">
+            {article.tag || "日常"}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
