799de77d3601c4439b38b1ae5dfd32865aa43ab8
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-19 09:27:35 +0900

    feat: 投稿一覧から投稿詳細ページへのリンクを作成

--- src/components/molecules/Article.tsx
+++ src/components/molecules/Article.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import Link from "next/link";
+import { FC, useCallback } from "react";
 import { Article } from "types/article";
 
 type Props = {
@@ -8,22 +9,37 @@ type Props = {
 const noImage = "/images/no-image.jpg";
 
 const Article: FC<Props> = ({ article }) => {
+  const trimTitle = useCallback((title: string) => {
+    let trimedTitle = title;
+
+    if (title.length > 40) {
+      trimedTitle = title.substring(0, 40) + "...";
+    }
+
+    return trimedTitle;
+  }, []);
   return (
     <div className=" rounded-xl shadow-xl overflow-hidden  md:w-[29%] m-3 w-full hover:cursor-pointer hover:scale-105 transition-all">
-      {/* Imageコンポーネントを使用することが望ましい、一時的にimgタグ　 */}
-      <img
-        src={article.eye_catch?.url || noImage}
-        alt="画像"
-        className="w-full max-h-52 object-cover"
-      />
-      <div className="p-6">
-        <div className="text-xl py-6">{article.title || "記事タイトル"}</div>
-        <div>
-          <span className=" bg-gray-300  rounded-full px-4 py-2">
-            {article.tag || "日常"}
-          </span>
-        </div>
-      </div>
+      <Link href={`/article/${article.id}`} passHref>
+        <a>
+          {/* Imageコンポーネントを使用することが望ましい、一時的にimgタグ　 */}
+          <img
+            src={article.eye_catch?.url || noImage}
+            alt="アイコン画像"
+            className="w-full max-h-44 sm:max-h-48  md:max-h-36 xl:max-h-52 object-cover"
+          />
+          <div className="px-5 mt-4 pt-2">
+            <div className="font-bold text-md mb-6">
+              {article.title ? trimTitle(article.title) : "記事タイトル"}
+            </div>
+            <div className="mb-4 ">
+              <span className=" bg-gray-300  rounded-full px-4 py-1">
+                {article.tag || "日常"}
+              </span>
+            </div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
