e808c40febbe69db2254021e1e372e6777415f9f
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-19 22:38:00 +0900

    feat: データ取得失敗時に404ページを表示するよう変更

--- src/pages/article/[id]/index.tsx
+++ src/pages/article/[id]/index.tsx
@@ -2,35 +2,46 @@ import { client } from "libs/client";
 import { GetServerSideProps } from "next";
 import React from "react";
 import { Article } from "types/article";
+import DefaultErrorPage from "next/error";
 
 type Props = {
-  article: Article;
+  article?: Article;
+  error?: any;
 };
 
 const noImage = "/images/no-image.jpg";
 
-export default function Index({ article }: Props) {
+export default function Index({ article, error }: Props) {
+  // ページ取得失敗時エラー
+  if (error) {
+    return (
+      <div>
+        <DefaultErrorPage statusCode={404} />
+      </div>
+    );
+  }
+
   return (
     <div className="container p-10 mx-auto">
       <div className=" max-w-5xl mx-auto">
         {/* Imageコンポーネントを使用することが望ましい、一時的にimgタグ　 */}
         <img
-          src={article.eye_catch?.url || noImage}
+          src={article?.eye_catch?.url || noImage}
           alt="アイキャッチ画像"
           className=" rounded-md w-full max-h-[25rem] object-cover aspect-video"
         />
         <div className="mt-10">
           <div className=" space-y-6">
             <h2 className=" text-blue-700 font-bold text-xl md:text-3xl">
-              {article.title}
+              {article?.title}
             </h2>
             <div>
               <span className=" bg-red-500 text-white  rounded-full px-6 py-2">
-                {article.tag || "日常"}
+                {article?.tag || "日常"}
               </span>
             </div>
             <p className=" text-gray-800 text-base md:text-lg">
-              {article.body}
+              {article?.body}
             </p>
           </div>
         </div>
@@ -40,16 +51,26 @@ export default function Index({ article }: Props) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const id = ctx.params?.id;
-  const idExceptArray = id instanceof Array ? id[0] : id;
-  const data = await client.get({
-    endpoint: "articles",
-    contentId: idExceptArray,
-  });
-
-  return {
-    props: {
-      article: data,
-    },
-  };
+  try {
+    const id = ctx.params?.id;
+    const idExceptArray = id instanceof Array ? id[0] : id;
+    const data = await client.get({
+      endpoint: "articles",
+      contentId: idExceptArray,
+    });
+    return {
+      props: {
+        article: data,
+      },
+    };
+  } catch (e) {
+    // データ取得失敗時エラーをpropsに渡す。
+    // 現在上手く渡せていないので修正が必要
+    const error = JSON.stringify(e);
+    return {
+      props: {
+        error,
+      },
+    };
+  }
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import Article from "components/molecules/Article";
 import { client } from "libs/client";
 import { Article as ArticleType } from "types/article";
+import DefaultErrorPage from "next/error";
 
 type Props = {
-  articles: ArticleType[];
+  articles?: ArticleType[];
+  error?: any;
 };
 
-export default function Home({ articles }: Props) {
+export default function Home({ articles, error }: Props) {
+  // ページ取得失敗時エラー
+  if (error) {
+    return (
+      <div>
+        <DefaultErrorPage statusCode={404} />
+      </div>
+    );
+  }
   return (
     <>
       <h1 className=" font-bold mx-auto container text-2xl p-5 ">記事一覧</h1>
       <div className="p-4 flex flex-wrap  max-w-5xl justify-between mx-auto after:content-[''] after:block after:w-[29%] after:m-3">
-        {articles.map((article) => {
+        {articles?.map((article) => {
           return <Article key={article.id} article={article} />;
         })}
       </div>
@@ -20,11 +30,22 @@ export default function Home({ articles }: Props) {
 }
 
 export const getServerSideProps = async () => {
-  const data = await client.get({ endpoint: "articles" });
+  try {
+    const data = await client.get({ endpoint: "articles" });
 
-  return {
-    props: {
-      articles: data.contents,
-    },
-  };
+    return {
+      props: {
+        articles: data.contents,
+      },
+    };
+  } catch (e) {
+    // データ取得失敗時エラーをpropsに渡す。
+    // 現在上手く渡せていないので修正が必要
+    const error = JSON.stringify(e);
+    return {
+      props: {
+        error,
+      },
+    };
+  }
 };
