eeb584b4a275f2b90b46212930bdd834ec2c11a0
kawanoRiku0 <riku.kawano20020518@gmail.com>
2022-01-19 09:25:26 +0900

    layout: ページタイトルのレイアウト調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Home({ articles }: Props) {
   return (
     <>
-      <h1 className="container text-2xl p-5 ">記事一覧</h1>
+      <h1 className=" font-bold mx-auto container text-2xl p-5 ">記事一覧</h1>
       <div className="container p-4 flex flex-wrap justify-between mx-auto after:content-[''] after:block after:w-[29%] after:m-3">
         {articles.map((article) => {
           return <Article key={article.id} article={article} />;
